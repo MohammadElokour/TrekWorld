8181006b90d829b8c7647c63914003d36f2e883b
MohammadElokour <mfelokour@gmail.com>
2019-06-15 02:49:46 +0300

    adding Login button

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -15,9 +15,45 @@ class HomeScreen extends StatelessWidget {
         ),
         header(),
         subHeader(),
+        login(),
       ],
     );
   }
+
+  Widget login() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 305.0, top: 660.0),
+      child: RawMaterialButton(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 9.0, horizontal: 21.0),
+          child: Text(
+            'Login',
+            style: TextStyle(
+              fontFamily: 'Trebuchet MS',
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        fillColor: Colors.brown[700],
+        splashColor: Colors.blueAccent,
+        shape: const StadiumBorder(),
+        onPressed: () {
+          print('tapped');
+        },
+      ),
+    );
+  }
+
+  // Widget login() {
+  //   return Padding(
+  //     padding: const EdgeInsets.only(left: 307.0, top: 650.0),
+  //     child: FloatingActionButton(
+  //       child: Text('Login'),
+  //       onPressed: () {},
+  //     ),
+  //   );
+  // }
 
   Widget header() {
     return Padding(
